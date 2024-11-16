7c3d7ab4876165961b16cc1e05888afdd5f9713e
lp <lp@DESKTOP-56HS7NB>
2024-11-16 14:13:25 +0530

    first push to fork has added

---
+++ Automation/src/shoba/AutomationTesting.java
@@ -0,0 +1,11 @@
+package shoba;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AutomationTesting {
+@Test
+public void demo() {
+	Reporter.log("fork has added",true);
+}
+}
