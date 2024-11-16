45baaf6937a8e78df50bc19fc7e62d0ab2191838
BOSUREDDY <BOSUREDDY@LAPTOP-H93NLDIP>
2024-11-16 14:07:34 +0530

    first push added fork

---
+++ Automation/src/Raj/Automation.java
@@ -0,0 +1,12 @@
+package Raj;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Automation {
+	@Test
+	public void demo() {
+		Reporter.log("addedfork",true);
+	}
+
+}
